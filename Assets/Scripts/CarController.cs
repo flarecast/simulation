022692718c7f223a0264b5cb802a44c6f29c70bd
JoRod94 <joao.rod94@gmail.com>
2016-04-30 20:08:12 +0100

    Added image chunk access and base communication code

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -7,51 +7,72 @@ using System.Net.Sockets;
 using System.Diagnostics;
 using System.Text;
 using System.Globalization;
+using System.Collections;
+using System.Collections.Generic;
 
 
 
 
 public class CarController : MonoBehaviour {
 
+	// TTL for all event messages
 	private static readonly int BASE_TTL = 5;
+
+	// Size of a grid square
+	private static readonly float GRID_ELEMENT_SIZE = 100F;
+
+	// Time between frames
+	private static readonly float FRAME_INTERVAL = 0.2F;
+
 	private static readonly String pythonAddress = "127.0.0.1";
-	private Socket pythonSocket;
+
+	// Events detected in the file are sent to the python detector through this socket
+	private Socket detectionSocket;
+
+	// Events from other threads are received through this socket.
+	// Events are forwarded through this socket
+	private Socket communicationSocket;
+
 	private byte[] receiveBuffer = new byte[1024];
 
+	// Variable for a positions file line
 	private string text;
+	// Positions file reader
 	private StreamReader reader;
 
+	// Map coordinate limits
 	private static readonly float minLat = 40.981F;
 	private static readonly float maxLat = 41.399F;
 	private static readonly float minLon = -8.795F;
 	private static readonly float maxLon = -8.358F;
 
+	// Center of the map
 	static readonly Vector3 center=   new Vector3(41F, -8.6F,0.0F);
 
+	// Image Proportions
+	public static float imWidth;
+	public static float imHeight;
 
-	private float imWidth;
-	private float imHeight;
-
+	// The car dot renderer
 	private SpriteRenderer rend;
 
+	// Variable indicating if the car should react to an external event
+	public bool react;
 
-	// Use this for initialization
-	public void Start () {
-	}
+
 
 	// Start on Demand
-	public void Begin (String filename, float width, float height) {
-		imWidth = width;
-		imHeight = height;
+	public void Begin (String filename) {
+		react = false;
 
+		//TODO: Review this port business
 		int port = 8000 + int.Parse (filename.Split ('.') [1]);
 		File.WriteAllText (Application.dataPath+"/port.txt", port.ToString());
 
-		//pythonSocket = GetSocket (port);
+		//detectionSocket = GetSocket(detectionPort)
+		//communicationSocket = GetSocket (communicationPort);
 
 		rend = GetComponent<SpriteRenderer>();
-
-
 
 		FileInfo f = new FileInfo (filename);
 		reader = f.OpenText();
@@ -60,31 +81,35 @@ public class CarController : MonoBehaviour {
 
 		transform.position = ConvertCoords (center.x, center.y);
 
-		// Use this to update every second, create UpdatePosition for that
-		//InvokeRepeating("UpdatePosition", 0, 1.0F);
+		// Use this to update in custom intervals, create UpdatePosition for that
+		InvokeRepeating("UpdatePosition", 0, FRAME_INTERVAL);
 
 		//SpawnPython ();
 
-		//pythonSocket.BeginReceive (receiveBuffer, 0, receiveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback),null );
+		// Receive external events
+		//communicationSocket.BeginReceive (receiveBuffer, 0, receiveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback),null );
 
 	}
 
+	// Handler for external event receptions
 	private void ReceiveCallback(IAsyncResult result){
-		int receivedLength = pythonSocket.EndReceive (result);
+		int receivedLength = communicationSocket.EndReceive (result);
 
 		byte[] sizedData = new byte[receivedLength];
 		Buffer.BlockCopy (receiveBuffer, 0, sizedData, 0, receivedLength);
 
-		int ttl = BitConverter.ToInt32 (sizedData,0);
+		//int ttl = BitConverter.ToInt32 (sizedData,0);
 
-		//TODO: Handle this and broadcast to close ones.
+		//TODO: If received id is my id, discard, otherwise, set react to true
+		if(/*content*/)
+			react = true;
 
-		pythonSocket.BeginReceive (receiveBuffer, 0, receiveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback),null );
+		//Continue receiving (events may be lost)
+		communicationSocket.BeginReceive (receiveBuffer, 0, receiveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback),null );
 	}
+		
 
-
-
-
+	// Spawns a python instance
 	private void SpawnPython(){
 		ProcessStartInfo psi = new ProcessStartInfo(); 
 		psi.FileName = "/bin/sh";
@@ -98,27 +123,43 @@ public class CarController : MonoBehaviour {
 		UnityEngine.Debug.Log(strOutput);
 	}
 
-	// Update is called once per frame
-	public void Update () {
-		text = reader.ReadLine();
+	//Called every 0.2 seconds
+	public void UpdatePosition () {
 
+		// React to external event
+		if (react) {
+			rend.color = new Color (0f, 0f, 1f);
+			react = false;
+			BroadcastNearby (/*possible args*/);
+		}
+
+
+		text = reader.ReadLine();
 		if (text != null && text != "NOP") {
 			string[] tokens = text.Split (' ');
 
 			double coordX = double.Parse (tokens [0], CultureInfo.InvariantCulture.NumberFormat);
 			double coordY = double.Parse (tokens [1], CultureInfo.InvariantCulture.NumberFormat);
 
-			//Debug.Log("X: "+x+" Y: "+y);
-
 			transform.position = ConvertCoords (coordX, coordY);
 
-			if (int.Parse (tokens [2]) == 0)
-				rend.color = new Color (0f, 1f, 0f);
-			else {
+
+			if(int.Parse (tokens [2]) == 1) {
 				rend.color = new Color (1f, 0f, 0f);
-				//pythonSocket.Send(StrToByteArray(coordX+" "+coordY));
-				//pythonSocket.Send(BitConverter.GetBytes(BASE_TTL));
+				detectionSocket.Send (/*content*/);
 			}
+			else
+				rend.color = new Color (0f, 1f, 0f);
+		}
+	}
+
+	private void BroadcastNearby(/*possible args*/){
+		for (int i = 0; i < CarManager.NR_CARS; i++) {
+			CarController car = CarManager.cars [i];
+			bool sameGridX = Math.Floor (car.transform.position.x / GRID_ELEMENT_SIZE) == Math.Floor (gameObject.transform.position.x / GRID_ELEMENT_SIZE);
+			bool sameGridY = Math.Floor (car.transform.position.y / GRID_ELEMENT_SIZE) == Math.Floor (gameObject.transform.position.y / GRID_ELEMENT_SIZE);
+			if (sameGridX && sameGridY)
+				car.communicationSocket.Send (/*content*/);
 		}
 	}
 
@@ -149,6 +190,18 @@ public class CarController : MonoBehaviour {
 	{
 		Encoding encoding = Encoding.UTF8;
 		return encoding.GetBytes(str);
+	}
+
+
+
+	// Use this for initialization (UNUSED, REPLACED BY Begin for On-Demand Calling)
+	public void Start () {
+	}
+
+
+	// Update is called once per frame
+	public void Update () {
+
 	}
 
 }
--- Assets/Scripts/CarManager.cs
+++ Assets/Scripts/CarManager.cs
@@ -6,16 +6,21 @@ public class CarManager : MonoBehaviour {
 
 	public static Object prefab = Resources.Load("Car");
 	public static readonly int NR_CARS = 3;
+	public static CarController[] cars = new CarController[NR_CARS];
 
 	// Use this for initialization
 	void Start () {
 
 		float width = gameObject.GetComponent<RectTransform> ().rect.width;
 		float height = gameObject.GetComponent<RectTransform> ().rect.height;
+		CarController.imWidth = width;
+		CarController.imHeight = height;
 
 		for (int i = 0; i < NR_CARS; i++) {
 			GameObject newCar = Instantiate (prefab) as GameObject;
-			newCar.GetComponent<CarController>().Begin("positions/positions." + i + ".txt", width, height);
+			CarController controller = newCar.GetComponent<CarController> ();
+			cars [i] = controller;
+			controller.Begin("positions/positions." + i + ".txt", width, height);
 		}
 	}
 	
