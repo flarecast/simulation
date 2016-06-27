b753de59ca8d8856583d3a48a4c2dd47f04da9c1
JoRod94 <joao.rod94@gmail.com>
2016-06-27 02:33:23 +0100

    Plugins now work correctly, this folder might be outdated sometimes from now on

---
+++ simulation-plugins/simulation-detection-plugin/simulation-detection-plugin.py
@@ -0,0 +1,28 @@
+import threading
+import socket
+from detector import *
+from event import *
+import os
+import struct
+
+class SimulationDetector(Detector):
+
+    @on_event
+    def interpret(self, lifetime):
+        return Event(1,1,"simulation", False, time.time(), lifetime, None)
+
+
+
+    def run(self):
+        s = socket.socket()
+        port = int(os.environ.get('FLARECAST_PORT')) #get port from env
+        host = '127.0.0.1'
+        s.bind((host, port))
+        s.listen(1)
+        unity_socket, addr = s.accept()
+
+        while True:
+            #lifetime = pickle.loads(unity_socket.recv(1024))
+            lifetime = struct.unpack('i', unity_socket.recv(4))[0]
+            print("LIFETIME: ", lifetime)
+            self.interpret(lifetime)
